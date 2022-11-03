3b89eca188fb3ae3f14bfc5fd569a57f96e7ed17
Helen Efebe <efebehelen95@gmail.com>
2022-11-03 16:53:32 +0100

    anchor tags

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,24 +5,37 @@ const Body = () => {
   
   return (
     <div id='link-container' className='link-container'>
-      <a href="https://twitter.com/HelenJonathan5" target="_blank" rel="noopener noreferrer" id='anchor-twitter' className='anchor' style={{color: "black"}}>
-        <button id="twitter" className='button'>Twitter Link</button>
-      </a>
-      <a href="https://training.zuri.team/" target="_blank" rel="noopener noreferrer" id='anchor-btn__zuri' className='anchor' style={{color: "black"}}>
-        <button id="btn__zuri" className='button'>Zuri Team</button>
-      </a>
-      <a href="http://books.zuri.team" target="_blank" rel="noopener noreferrer" id='anchor-books' className='anchor' style={{color: "black"}}>
-        <button id="books" className='button'>Zuri Books</button>
-      </a>
-      <a href="https://books.zuri.team/python-for-beginners?ref_id=<Helen>" target="_blank" rel="noopener noreferrer" id='anchor-book_python' className='anchor' style={{color: "black"}}>
-        <button id="book_python" className='button'>Python Books</button>
-      </a>
-      <a href="https://backgroundcheck.zuri.team/" target="_blank" rel="noopener noreferrer" id='anchor-pitch' className='anchor' style={{color: "black"}}>
-        <button id="pitch" className="button">Background check for coders</button>
-      </a>
-      <a href="https://books.zuri.team/design-rules" target="_blank" rel="noopener noreferrer" id='anchor-btn__design' className='anchor' style={{color: "black"}}>
-        <button id="btn__design" className="button">Design Books</button>
-      </a>
+      <div className='button'>
+        <a href="https://twitter.com/HelenJonathan5" target="_blank" rel="noopener noreferrer" id="twitter" className='button' >
+          Twitter Link
+        </a>
+      </div>
+      <div className='button'>
+        <a href="https://training.zuri.team/" target="_blank" rel="noopener noreferrer" id="btn__zuri" className='anchor' >
+          Zuri Team
+        </a>
+      </div>
+      <div className='button'>
+        <a href="http://books.zuri.team" target="_blank" rel="noopener noreferrer" id="books" className='anchor' >
+          Zuri Books
+        </a>
+      </div>
+      <div className='button'> 
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=<Helen>" target="_blank" rel="noopener noreferrer" id="book_python" className='anchor' >
+          Python Books
+        </a>
+      </div>
+      <div className="button">
+        <a href="https://backgroundcheck.zuri.team/" target="_blank" rel="noopener noreferrer" id="pitch" className='anchor' >
+          Background check for coders
+        </a>
+      </div>
+      <div className="button">
+        <a href="https://books.zuri.team/design-rules" target="_blank" rel="noopener noreferrer" id="btn__design" className='anchor' >
+          Design Books
+        </a>
+      </div>
+      
     </div>
   );
 }
